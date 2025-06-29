002643bb7e5d79ef388dfdedd439e952d303e5bf
271915902 <271915902@qq.com>
2025-06-29 13:23:31 +0800

    refactor(router): 实现自动注册路由功能以简化代码
    移除手动注册路由的方式，改为通过扫描目录自动注册所有以`.router.js`结尾的路由文件

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,12 @@
 const Koa = require("koa");
-const userRouter = require("../router/user.router");
-const loginRouter = require("../router/login.router");
+
 const bodyParser = require("koa-bodyparser");
+const {registerRouter} = require("../router/index");
 // 创建app
 const app = new Koa();
 
 app.use(bodyParser());
 
-app.use(userRouter.routes());
-app.use(userRouter.allowedMethods());
-app.use(loginRouter.routes());
-app.use(loginRouter.allowedMethods());
+registerRouter(app);
 
 module.exports = app;
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+// 自动注册路由
+function registerRouter(app) {
+  const files = fs.readdirSync(path.resolve(__dirname));
+  for (const file of files) {
+    if (!file.endsWith(".router.js")) continue;
+    const router = require(`./${file}`);
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+  }
+}
+module.exports = {
+  registerRouter,
+};
