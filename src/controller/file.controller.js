5d62f42edb63f8a0abae249ab0b6218a88e1fcff
271915902 <271915902@qq.com>
2025-07-03 12:06:25 +0800

    feat(文件上传): 实现头像上传功能
    添加文件服务处理头像上传逻辑，修改控制器返回上传结果

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -1,6 +1,14 @@
+const FileService = require("../service/file.service");
 class FileController {
   async create(ctx, next) {
-    ctx.body = "上传成功";
+    const { filename, mimetype, size } = ctx.request.file;
+    const { id } = ctx.user;
+    const result = await FileService.create(filename, mimetype, size, id);
+    ctx.body = {
+      code: 0,
+      data: result,
+      message: "头像上传成功",
+    };
   }
 }
 
